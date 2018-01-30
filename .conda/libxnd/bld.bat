4f4c5615be8a8b1eeaf839f34aa9ae225a2cb246
Stefan Krah <skrah@bytereef.org>
2018-01-30 17:34:53 +0100

    Add standalone conda library build.

---
+++ .conda/libxnd/bld.bat
@@ -0,0 +1,5 @@
+cd "%RECIPE_DIR%\..\..\" || exit 1
+call vcbuild64.bat || exit 1
+call runtest64.bat || exit 1
+copy /y dist64\lib* "%PREFIX%\Library\bin\"
+copy /y dist64\ndtypes.h "%PREFIX%\Library\include\"
