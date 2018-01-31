0dc92bac8ec2d1e54279f0e57965fc7b04242ab4
Stefan Krah <skrah@bytereef.org>
2018-01-31 16:02:23 +0100

    Fix path.

--- .conda/libxnd/bld.bat
+++ .conda/libxnd/bld.bat
@@ -1,4 +1,4 @@
-cd "%RECIPE_DIR%\..\..\" || exit 1
+cd "%RECIPE_DIR%\..\..\vcbuild" || exit 1
 call vcbuild64.bat || exit 1
 call runtest64.bat || exit 1
 copy /y dist64\lib* "%PREFIX%\Library\bin\"
