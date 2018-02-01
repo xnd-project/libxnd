2bd32514f0bf5a48c7192a1b621c9ce8c6bf3e03
Stefan Krah <skrah@bytereef.org>
2018-02-01 13:47:58 +0100

    Fix the conda build.

--- .conda/libxnd/build.sh
+++ .conda/libxnd/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-cd $RECIPE_DIR/../../ || exit 1
-./configure --without-docs --prefix=$PREFIX || exit 1
-make check
+cd "$RECIPE_DIR/../../" || exit 1
+./configure --prefix="$PREFIX" --with-includes="$PREFIX/include" --with-libs="$PREFIX/lib" --without-docs || exit 1
+make check || exit 1
 make install
