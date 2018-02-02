6b59d3cb44d7b739b67c3c5fbf82bcda756a4cbb
Stefan Krah <skrah@bytereef.org>
2018-02-02 14:11:27 +0100

    Change syntax (env vars are probably empty).

--- .maybe_deploy.sh
+++ .maybe_deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ X"$TRAVIS_BRANCH" = X"master" ] && [ X"$TRAVIS_PULL_REQUEST" = X"false" ]; then
   anaconda --token $ANACONDA_NDTYPES_TOKEN upload $LIBNDTYPES --user plures --channel plures &&
   anaconda --token $ANACONDA_NDTYPES_TOKEN upload $NDTYPES --user plures --channel plures &&
   anaconda --token $ANACONDA_XND_TOKEN upload $LIBXND --user plures --channel plures &&
-  anaconda --token $ANACONDA_XND_TOKEN upload $XND --user plures --channel plures
+  anaconda --token $ANACONDA_XND_TOKEN upload $XND --user plures --channel plures &&
+  echo "\nAnaconda uploads successful.\n"
+else
+  echo "\nAnaconda uploads not triggered.\n"
 fi
