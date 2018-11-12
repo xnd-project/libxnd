25302947d1cba6a78baa8043da0d15380e812127
Stefan Krah <skrah@bytereef.org>
2018-11-12 14:40:08 +0100

    Add tests for nested variable dimensions.

--- python/test_xnd.py
+++ python/test_xnd.py
@@ -863,6 +863,42 @@ class TestVarDim(XndTestCase):
                     y[indices] = w
                     self.assertNotStrictEqual(x, y)
 
+    def test_var_dim_nested(self):
+
+        t = "var(offsets=[0,2]) * var(offsets=[0,1,3]) * (var(offsets=[0,2]) * var(offsets=[0,3,7]) * float32, int64)"
+        v = [[([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0, 6.0]], -1)],
+             [([[7.0, 8.0, 9.0], [10.0, 11.0, 12.0, 13.0]], -2),
+              ([[14.0, 15.0, 16.0], [17.0, 18.0, 19.0, 20.0]], -3)]]
+
+        x = xnd(v, type=t)
+
+        self.assertEqual(x, v)
+        self.assertEqual(x.value, v)
+
+        self.assertEqual(x[0], v[0])
+        self.assertEqual(x[1], v[1])
+
+        self.assertEqual(x[0,0], v[0][0])
+        self.assertEqual(x[1,0], v[1][0])
+        self.assertEqual(x[1,1], v[1][1])
+
+        self.assertEqual(x[0,0,0], v[0][0][0])
+        self.assertEqual(x[0,0,1], v[0][0][1])
+
+        self.assertEqual(x[1,0,0], v[1][0][0])
+        self.assertEqual(x[1,0,1], v[1][0][1])
+
+        self.assertEqual(x[1,1,0], v[1][1][0])
+        self.assertEqual(x[1,1,1], v[1][1][1])
+
+        self.assertEqual(x[0,0,0,0], v[0][0][0][0])
+        self.assertEqual(x[0,0,0,1], v[0][0][0][1])
+
+        self.assertEqual(x[0,0,0,0], v[0][0][0][0])
+        self.assertEqual(x[0,0,0,1], v[0][0][0][1])
+
+        self.assertEqual(x[1,0,0,0,1], v[1][0][0][0][1])
+        self.assertEqual(x[1,0,0,0,1], v[1][0][0][0][1])
 
 class TestSymbolicDim(XndTestCase):
 
